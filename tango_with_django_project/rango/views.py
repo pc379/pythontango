b8eddd40a3710d7ca0fd3999c44111a0c6448c54
pc379 <pc379@cornell.edu>
2014-07-22 16:51:58 -0400

    midway through chpater 4
    could not get index page to work

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,6 +1,14 @@
 from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render_to_response
 
 def index(request):
-    return HttpResponse("Rango says hello world!")
+    #request context of the request, contains information about client machine details, etc.
+    context = RequestContext(request)
+
+    context_dict = {'boldmessage': "I am bold font from the context"}
+
+    return render_to_response('rango/index.html', context-dict, context)
+
 
 
--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -11,7 +11,15 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+SETTINGS_DIR = os.path.dirname(__file__)
 
+print "base directory is" + BASE_DIR
+print SETTINGS_DIR
+
+PROJECT_PATH = os.path.join(SETTINGS_DIR, os.pardir)
+print "project path "+ PROJECT_PATH
+PROJECT_PATH = os.path.abspath(PROJECT_PATH)
+print "project path2 "+ PROJECT_PATH
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
@@ -83,3 +91,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+TEMPLATE_PATH = os.path.join(PROJECT_PATH, 'templates')
+
+TEMPLATE_DIRS = (
+    TEMPLATE_PATH,
+
+)
