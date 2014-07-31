22a7bd72b605327a904f01923651fafd69459b7f
pc379 <pc379@cornell.edu>
2014-07-31 17:03:56 -0400

    chapter 4 exercise
    half way done. bug fixed. static image, not done

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns,url
 from rango import views
 
-urlpatterns=patterns('',
-                      url(r'^$', views.index, name='index'),
-                      url(r'^rango/about/$', views.about, name='about'))
+urlpatterns=[
+    url(r'^$', views.index, name='index'),
+    url(r'^about/$', views.about, name='about'),
+]
 
 print urlpatterns
 
